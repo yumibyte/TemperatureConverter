f8b37e5e4b18152bab64ec11c6b2294edd353045
Yumibyte <ashraigosa@gmail.com>
2020-11-04 15:57:04 -0800

    menu setup

---
+++ src/GUI.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+
+public class GUI {
+
+    private static JLabel temperatureInputType;
+    private static JLabel temperatureOutputType;
+    private static JLabel temperatureValue;
+    private static JLabel temperatureResult;
+
+    private static JComboBox temperatureInputTypeUser;
+    private static JTextField temperatureOutputTypeUser;
+    private static JTextField temperatureValueUser;
+
+    public static void main(String[] args) {
+        // set up basic frame/panel
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        frame.setSize(450, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setLayout(null);
+
+        // set up any buttons/labels/input areas
+        //temperature value input
+        temperatureValue = new JLabel("Temperature value: ");
+        temperatureValue.setBounds(20, 20, 200, 20);
+        panel.add(temperatureValue);
+
+        temperatureValueUser = new JTextField();
+        temperatureValueUser.setBounds(20, 50, 200, 20);
+        panel.add(temperatureValueUser);
+
+        // temperature input type
+        temperatureInputType = new JLabel("Temperature unit: ");
+        temperatureInputType.setBounds(20, 100, 200, 20);
+        panel.add(temperatureInputType);
+
+        String[] temperatureTypes = {"Celsius", "Kelvin", "Rankine", "Fahrenheit"};
+        temperatureInputTypeUser = new JComboBox(temperatureTypes);
+        temperatureInputTypeUser.setBounds(20, 130, 200, 20);
+        panel.add(temperatureInputTypeUser);
+
+
+
+
+
+
+        frame.setVisible(true);
+    }
+}
